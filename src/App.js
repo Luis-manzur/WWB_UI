2e132bb2bcc42269245fd48416339f45a71223d4
Luis Manzur <luismanzur91@outlook.com>
2022-03-23 14:30:55 -0400

    Nav + Login + SignUp + Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import NavigationBar from './Components/NavigationBar';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// Components
 import HomePage from './Pages/HomePage';
+import SingUp from './Components/SingUp';
+import LogIn from './Components/LogIn'
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
-      <HomePage></HomePage>
+      <Router>
+          <NavigationBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path='/register' element={<SingUp />} />
+          <Route path='/login' element={<LogIn />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Nav, Navbar } from 'react-bootstrap';
-import styles from './NavigationBar.module.css'
+import { Nav, Navbar, Button, Container} from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import './NavigationBar.css'
 
 export default function NavigationBar() {
+  const navigate = useNavigate();
+
+  const HandleLogin = () => {
+    navigate('/login')
+  }
   return (
-    <Navbar bg = "dark" variant='dark' sticky='top' expand ='lg' collapseOnSelect>
-        <Navbar.Brand>
-            <img className = {styles.navBarLogo} src = 'https://logowik.com/content/uploads/images/free-letter-n-logo-template3672.jpg'/>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-            <Nav>
-                <Nav.Link href='/'>Home</Nav.Link>
-                <Nav.Link href='/aboutus'>Quienes somos</Nav.Link>
-                <Nav.Link href='contactus'>Contáctanos</Nav.Link>
-                <Nav.Link href='login'>Iniciar sesión</Nav.Link>
-                <Nav.Link href = 'signup'>Registrarse</Nav.Link>
-                <Nav.Link href = 'logout'>Cerrar sesión</Nav.Link>
-            </Nav>
+    <Navbar bg="dark" variant='dark' sticky='top' expand='lg' collapseOnSelect>
+      <Container fluid>
+        <Navbar.Brand href="/">World Wide Booking</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            navbarScroll
+          >
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/search">Search</Nav.Link>
+            <Nav.Link href="/about">About us</Nav.Link>
+          </Nav>
+          <Button className="d-flex" onClick={HandleLogin}>
+            Log in
+          </Button>
         </Navbar.Collapse>
+      </Container>
     </Navbar>
   )
 }
